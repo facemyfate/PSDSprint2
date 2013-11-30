b09f979747a8d32a88786352d67c8ba9f254bcd3
facemyfate <facemyfate@live.com>
2013-11-30 12:43:30 +0800

    Change test to test123

--- src/main/java/com/helloWorld/sprint2/HomeController.java
+++ src/main/java/com/helloWorld/sprint2/HomeController.java
@@ -33,7 +33,7 @@ public class HomeController {
 		
 		model.addAttribute("serverTime", formattedDate );
 		
-		String test = "test";
+		String test = "test123";
 		
 		model.addAttribute("test", test);
 		
