deb2bc14f6b091c127719e9434d620915ec8e468
facemyfate <facemyfate@live.com>
2013-11-30 12:28:03 +0800

    Test

--- src/main/java/com/helloWorld/sprint2/HomeController.java
+++ src/main/java/com/helloWorld/sprint2/HomeController.java
@@ -33,6 +33,10 @@ public class HomeController {
 		
 		model.addAttribute("serverTime", formattedDate );
 		
+		String test = "test";
+		
+		model.addAttribute("test", test);
+		
 		return "home";
 	}
 	
